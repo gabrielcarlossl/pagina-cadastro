293f0ef9ab0cdfaa2efd132299de4a5e4723ae62
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2022-07-14 13:18:32 -0300

    Criação dos componentes Layout e titulo

---
+++ next-crud/src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import Titulo from '../components/Titulo'
+
+interface LayoutProps{
+    titulo: string
+    children: any
+}
+
+export default function Layout(props: LayoutProps){
+    return (
+        <div className={`
+        flex flex-col w-2/3
+        bg-white text-gray-800 
+        rounded-md
+        `}>
+            <Titulo>{props.titulo}</Titulo>
+            <div className={` 
+            px-10 p-6 
+            `}>
+                {props.children}
+            </div> 
+        </div>
+    )
+}
--- next-crud/tailwind.config.js
+++ next-crud/tailwind.config.js
@@ -1,28 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "src/pages/**/*.{js,ts,jsx,tsx}",
+  
+    "src/components/**/*.{js,ts,jsx,tsx}",
+    
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
 
-module.exports = {
-
-  content: [
-  
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  
-  "./components/**/*.{js,ts,jsx,tsx}",
-  
-  ],
-  
-  theme: {
-  
-  extend: {},
-  
-  },
-  
-  plugins: [],
-  
-  }
